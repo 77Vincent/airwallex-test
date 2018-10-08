b8fd5dbab8742dbb84044a232557bce701458298
Vincent77 <wentianqi77@outlook.com>
2018-10-08 18:26:17 +0800

    1. add componets 2.add node-sass

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.scss'
+
+export default (props) => {
+  return (
+    <header className="App-header">
+      <div className="App-header-logo">BROCCOLI & CO.</div>
+    </header>
+  )
+}
