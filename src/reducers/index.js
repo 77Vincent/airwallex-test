4aac6fd43ee22755d07e7f7afa4fce8f72c5a9a9
vwen <Vincent.Wen@adp.com>
2018-10-11 17:51:50 +0800

    extract child reducer from index of the reducers, button style

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,6 @@
 import { combineReducers } from 'redux'
 
-import { PUT, TOGGLE } from '../actions/types'
-
-const initialState = {
-  requestFormDisplayed: true,
-  requestSent: false,
-}
-
-const requestInviteForm = (state = initialState, action) => {
-  switch(action.type) {
-    default:
-      return state
-    case PUT:
-      return {
-        ...state,
-        requestSent: action.payload,
-      }
-    case TOGGLE:
-      return {
-        ...state,
-        requestFormDisplayed: action.payload,
-      } 
-  }
-}
+import { default as requestInviteForm } from './requestInviteForm'
 
 const rootReducer = combineReducers({
   requestInviteForm,
---
+++ src/reducers/requestInviteForm.js
@@ -0,0 +1,23 @@
+import { PUT, TOGGLE } from '../actions/types'
+
+const initialState = {
+  requestFormDisplayed: true,
+  requestSent: false,
+}
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+    case PUT:
+      return {
+        ...state,
+        requestSent: action.payload,
+      }
+    case TOGGLE:
+      return {
+        ...state,
+        requestFormDisplayed: action.payload,
+      } 
+  }
+}
